59ecd39bd8175904624615d2f2fca65b6dcd3c8f
pypypypypyp <natch7th@gmail.com>
2020-03-27 00:47:47 +0900

    20200327

--- straighten.py
+++ straighten.py
@@ -20,16 +20,18 @@ import multiprocessing
 
 pyfftw.config.NUM_THREADS = 4
 pyfftw.interfaces.cache.enable()
+ALLOWED_ANGLE = 45
+BSIZE = 1.5
 
 def setupParserOptions():
-    parser = optparse.OptionParser(usage="Usage: %prog <3D volume> [options]")
+    parser = optparse.OptionParser(usage="Usage: %prog <filtered volume> <raw volume> [options]")
     parser.add_option('--thres', dest='thres', action='store', type='float', default=0.05, help='thresholding value to detect filaments')
-    parser.add_option('--radius', dest='radius', action='store', type='int', default=26, help='radius of a filament')
-    parser.add_option('--largeness', dest='largeness', action='store', type='int', default=5, help='a value determining how large a filament should be')
-    parser.add_option('--average', dest='average', action='store', type='int', default=10, help='a value determining how many images are averaged to calculate the center of a filament cross section')
+    parser.add_option('--radius', dest='radius', action='store', type='int', default=39, help='radius of a filament')
+    parser.add_option('--largeness', dest='largeness', action='store', type='int', default=15, help='how large a filament should be?')
+    parser.add_option('--average', dest='average', action='store', type='int', default=40, help='how many images are averaged to calculate the center of a filament cross section?')
     options, args = parser.parse_args()
-    if len(args) > 1: parser.error("Unknown command-line options: %s"%str(args))
-    if len(sys.argv) < 2:
+    if len(args) > 2: parser.error("Unknown command-line options: %s"%str(args))
+    if len(sys.argv) < 3:
         parser.print_help()
         sys.exit()
     params = {}
@@ -37,17 +39,6 @@ def setupParserOptions():
         if isinstance(i.dest, str): params[i.dest] = getattr(options, i.dest)
     return params
 
-def make_yslices(data, thickness, overlap):
-    y = data.shape[1]
-    slices = (y-overlap) / (thickness-overlap)
-    stack = []
-    y_values = []
-    for i in range(slices):
-        yy = thickness*i + thickness/2 - overlap*i
-        sl = np.array([np.sum(data[:, int(yy-thickness/2):int(yy+thickness/2), :], axis=1)])
-        stack.append(sl)
-        y_values.append(yy)
-    return stack, y_values
 def convolve2d(A, B, mode="same"):
         # determine output array shape (Y, X) and calculation array shape (L_calc, M_calc)
         A = A.astype(np.float32)
@@ -79,9 +70,11 @@ def convolve2d(A, B, mode="same"):
 def load_volume(filename):
         # input volume should have white objects with black background
         print "Loading the volume: %s ..."%filename
-        e = EMData(sys.argv[1])
+        e = EMData(filename)
         data = EMNumPy.em2numpy(e)
         data = data.astype(np.float32)
+        data *= -1
+        data -= data.mean()
         print " -> complete!"
         return data
 
@@ -125,7 +118,7 @@ def meijering(image, sigma, black_ridges=False):
         vectx /= vectlen
         vecty /= vectlen
         valL = np.where(L > 0, 0, L)
-        mask = np.abs(vecty) < sqrt(2)/2 # make sure to remove unnecessary signals oriented close to the x-axis
+        mask = np.abs(vecty) < np.cos(np.deg2rad(ALLOWED_ANGLE)) # make sure to remove unnecessary signals oriented close to the z-axis
         valL[mask] = 0
         valL = divide_nonzero(valL, np.min(valL))
         vect = np.array([vectx, vecty])
@@ -173,12 +166,13 @@ def remove_small_objects(meij, outer_radius, largeness=10, thres=0.05):
         print " -> complete!"
         return meij
 
-def remove_objects_close_to_edges(meij, outer_radius):
-        print "Removing objects too close to edges ..."
+def remove_near_edge_objects(meij, outer_radius):
+        print "Removing near-edge objects ..."
         labels, n = label(meij)
         centers = center_of_mass(meij, labels, range(1, n+1))
+        m = 3
         for i, center in enumerate(centers):
-                if outer_radius*2 < center[0] and center[0] < meij.shape[0]-outer_radius*2 and outer_radius*2 < center[1] and center[1] <  meij.shape[1]-outer_radius:
+                if outer_radius*m < center[0] and center[0] < meij.shape[0]-outer_radius*m and outer_radius*m < center[1] and center[1] <  meij.shape[1]-outer_radius*n:
                         pass
                 else:
                         labels[labels == i+1] = 0
@@ -198,27 +192,31 @@ def FDAGK(sigma, theta, rho):
         FDAGK = -1 * ((x*np.cos(theta)+y*np.sin(theta)) / sigma**2) * AGK
         return FDAGK
 
-def find_filament_axis(proj, meij, outer_radius):
+def find_filament_axis(proj, meij, vect, outer_radius):
         print "Finding the axis for each object in the projection image ..."
         labels, n = label(meij)
         axes = [[] for i in range(n)]
         print " -> roughly estimating the central points of the object along the x-axis of the image ..."
+        print " -> working on %d objects ..."%n
+        deltas = [0] * n
         for x in range(meij.shape[0]):
                 for obj_id in range(1, n+1):
                         # find the center of the object (obj_id) at this x-coordinate level
                         labels_x = labels[x]
                         meij_x = meij[x]
+                        positions_x = np.arange(meij_x.size)
                         # pick up a region of the filtered image containing the object
                         labels_x_equals_obj_id = labels_x == obj_id
                         meij_x_id = meij_x[labels_x_equals_obj_id]
+                        positions_x_id = positions_x[labels_x_equals_obj_id]
                         if len(meij_x_id) == 0: continue # if this level doesn't contain the object, move to the next object
-                        patch_start = np.arange(meij_x.size)[labels_x_equals_obj_id][0]
-                        filt = maximum_filter(meij_x_id, size=outer_radius*2)
+                        filt = maximum_filter(meij_x_id, size=outer_radius)
                         peaks = np.where(filt == meij_x_id)[0]
-                        peaks += patch_start
+                        peaks = positions_x_id[peaks]
                         # several peaks may be found, so the peak closest to the previous one is chosen
                         if len(axes[obj_id-1]) != 0:
                                 prev_peak_pos = axes[obj_id-1][-1]
+                                prev_peak_x = prev_peak_pos[0]
                                 prev_peak_y = prev_peak_pos[1]
                                 peaks_diff = np.abs(peaks - prev_peak_y)
                                 peak_index = np.where(peaks_diff == np.min(peaks_diff))[0][0]
@@ -231,90 +229,43 @@ def find_filament_axis(proj, meij, outer_radius):
         print " -> complete!"
         return axes
 
-def tmp():
-        print " -> finding accurate centeral positions ..."
-        height = outer_radius * 6
-        width = outer_radius * 4
-        dstep = 10
-        angstep = 10
-        angrange = 2
-        new_axes = []
-        for axis in axes:
-                new_x = []
-                new_y = []
-                x, y = np.array(axis).transpose()
-                for i in range(0, x.size, dstep):
-                        x_range = x[i:i+dstep]
-                        y_range = y[i:i+dstep]
-                        x_pos = x_range.mean()
-                        y_pos = y_range.mean()
-                        theta = np.arctan2(y_range[-1]-y_range[0], x_range[-1]-x_range[0]) - np.pi/2
-                        X, Y = np.meshgrid(np.arange(-width/2, width/2), np.arange(-height/2, height/2))
-                        isum = np.zeros(width)
-                        for angscale in range(-angrange, angrange+1):
-                                angle = theta + angstep*angscale/180.*np.pi
-                                xx = y_pos + X*np.sin(angle) + Y*np.cos(angle)
-                                yy = x_pos + X*np.cos(angle) - Y*np.sin(angle)
-                                ptcl = map_coordinates(proj, [yy, xx], order=0).reshape(xx.shape)
-                                EMNumPy.numpy2em(ptcl).write_image("ptcl.mrcs", -1)
-                                tsum = np.sum(ptcl, axis=0)
-                                if isum.ptp() < tsum.ptp():
-                                        theta = angle
-                                        isum = tsum
-                        isum -= isum.mean()
-                        cor = np.correlate(isum, isum[::-1], mode="same")
-                        plt.plot(isum)
-                        plt.savefig("tmp/%d_%d.png"%(len(axis), i))
-                        plt.clf()
-                        cor = cor[cor.size/2-outer_radius:cor.size/2+outer_radius]
-                        shift = (np.argmax(cor) - cor.size/2.)/2.
-                        print theta*180/np.pi, shift
-                        x_pos -= np.sin(theta+np.pi/2)*shift
-                        y_pos += np.cos(theta+np.pi/2)*shift
-                        new_x.append(x_pos)
-                        new_y.append(y_pos)
-                new_axes.append((np.array(new_x), np.array(new_y)))
-        print " -> calculating a spline curve for each object ..."
-        interp_axes = []
-        for axis in new_axes:
-                x, y = axis
-                f = interp1d(x, y)
-                newx = np.arange(x[0], x[-1])
-                newy = f(newx)
-                interp_axes.append((newx, newy))
-        return interp_axes
-
 def find_3D_axis(data, twod_axes, outer_radius, ave=10):
         print "Finding the axis for each object in the volume data ..."
-        rad = int(outer_radius * 1.5)
+        rad = int(outer_radius*2)
+        axes = []
         for i, twod_axis in enumerate(twod_axes):
                 print " -> calculating for object #%d ..."%(i+1)
                 centers = []
-                f = open("test%d.txt"%i, "w")
                 y_positions, z_positions = twod_axis
+                #f = open("test.txt", "w")
                 for j in range(len(y_positions)/ave):
                         ys = y_positions[j*ave:(j+1)*ave]
                         zs = z_positions[j*ave:(j+1)*ave]
                         z = int(zs.mean())
+                        if z-rad < 0 or z+rad < data.shape[1]: continue
                         patch = data[ys[0]:ys[-1]+1, :, z-rad:z+rad].sum(axis=0)
                         patch = cutoff_noise(patch, c_value=5, verbose=False)
-                        center_x, center_z = find_center(patch)
+                        EMNumPy.numpy2em(patch).write_image("test.mrcs", -1)
+                        center_x, center_z = find_center(patch, outer_radius/2)
                         center_z = z-rad+center_z
                         center_y = ys.mean()
-                        centers.append((center_x, center_y, center_z))
-                        f.write("%d %d %d\n"%(center_z, center_x, center_y))
-                        print center_z, center_x, center_y
-                f.close()
+                        if outer_radius*BSIZE < center_x and center_x < data.shape[1] - outer_radius*BSIZE:
+                                centers.append((center_x, center_y, center_z))
+                                #f.write("%d %d %d\n"%(center_z, center_x, center_y))
+                #f.close()
+                centers = np.array(centers).transpose()
+                axes.append(centers)
         print " -> complete!"
         return axes
 
-def find_center(data):
+def find_center(data, limit):
         data -= data.min()
         thres = data.mean() - data.std()
         data[data < thres] = thres
         data -= data.mean()
         data_inv = data[::-1, ::-1]
         cor = correlate2d(data, data_inv, mode="same")
+        cor = cor[cor.shape[0]/2-limit*2:cor.shape[0]/2+limit*2, cor.shape[1]/2-limit*2:cor.shape[1]/2+limit*2]
         max_pos = np.argmax(cor)
         cor_Y, cor_X = cor.shape
         dy = (max_pos/cor_X - cor_Y/2)/2.
@@ -324,9 +275,44 @@ def find_center(data):
         plt.imshow(data)
         return center_y, center_x
 
-def create_straight_volume(data, axes):
+def create_straight_volume(data, axes, outer_radius):
+        vols = []
+        od = int(outer_radius*BSIZE)
         for axis in axes:
-                
+                x, y, z = axis
+                fx = interp1d(y, x)
+                fz = interp1d(y, z)
+                newy = np.arange(y[0], y[-1])
+                newx = fx(newy)
+                newz = fz(newy)
+                vol = np.zeros((len(newy), od*2, od*2))
+                for i, y in enumerate(newy):
+                        y = int(y)
+                        newx_i = int(newx[i])
+                        newz_i = int(newz[i])
+                        vol[i] = data[y, newx_i-od:newx_i+od, newz_i-od:newz_i+od]
+                vols.append(vol)
+        return vols
+
+def remove_near_edge_signals(meij, outer_radius):
+        print "Removing near-edge signals ..."
+        y_size, x_size = meij.shape
+        mask = np.ones((y_size, x_size)).astype(np.bool)
+        x_offset = outer_radius*2
+        y_offset = outer_radius*2
+        x_size -= outer_radius * 4
+        y_size -= outer_radius * 4
+        mask[y_offset:y_offset+y_size, x_offset:x_offset+x_size] = False
+        
+        meij[mask] = 0
+        print " -> complete!"
+        return meij
+
+def save_volumes(vols):
+        print "Saving the volumes ..."
+        for i, vol in enumerate(vols):
+                save("%s.vol%d.mrc"%(sys.argv[2], i), vol)
+        print " -> complete!"
 
 def main():
         # params
@@ -337,17 +323,21 @@ def main():
         data = load_volume(sys.argv[1])
         # create a projection image
         proj = create_projection(data, axis=1) # along x-axis
+        save("proj.mrc", proj)
         # cut-off noise and normalize
-        proj = cutoff_noise(proj, c_value=5)
+        proj = cutoff_noise(proj, c_value=3)
         proj = normalize(proj)
         # meijering filter
         meij, vect = meijering(proj, outer_radius)
+        # objects must not include near-edge regions
+        meij = remove_near_edge_signals(meij, outer_radius)
+        save("meij.mrc", meij)
         # remove small objects
         meij = remove_small_objects(meij, outer_radius, largeness=params["largeness"], thres=params["thres"])
         # remove islands too close to edges
-        meij = remove_objects_close_to_edges(meij, outer_radius)
+        meij = remove_near_edge_objects(meij, outer_radius)
         # finding the filament axis for each object in the projection image
-        twod_axes = find_filament_axis(proj, meij, outer_radius)
+        twod_axes = find_filament_axis(proj, meij, vect, outer_radius)
         plt.imshow(proj)
         for obj_axis in twod_axes:
                 x, y = obj_axis
@@ -356,7 +346,10 @@ def main():
         # determine three-dimensioal axis for each object
         axes = find_3D_axis(data, twod_axes, outer_radius, ave=params["average"])
         # create straight volumes
-        vols = create_straight_volume(data, axes)
+        data = load_volume(sys.argv[2])
+        vols = create_straight_volume(data, axes, outer_radius)
+        # save!
+        save_volumes(vols)
 
 if __name__ == "__main__":
         main()
